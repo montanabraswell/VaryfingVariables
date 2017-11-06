eb5b94f498a6ee8a258b9a2cde46dc3977f164b7
montanabraswell <montanabraswell1@gmail.com>
2017-11-06 15:58:00 -0500

    Add files via upload

---
+++ VaryfingVariables/src/varyfingvariables/VaryfingVariables.java
@@ -0,0 +1,47 @@
+/* MOOC Java Tutorial 4
+ * Java Tutorial 4
+ * Montana Braswell
+ * 10/26/2017
+ */
+package varyfingvariables;
+
+/**
+ *
+ * @author Montana
+ */
+public class VaryfingVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // making the variables to 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String text = "Chickens";
+        String text2 ="Bacon(kg)";
+        String text3 = "A Tractor:";
+        String text4 = "In a nutshell";
+        String text5 = "There is None!";
+        String text6 = "Zetor";
+        int wholeNumber =9000;
+        double decimalNumber = 0.1;
+        boolean isTrue = true;
+        
+        System.out.print(text + ":");
+        System.out.println();
+        System.out.println(wholeNumber);
+        System.out.print(text2 + ":");
+        System.out.println();
+        System.out.println(decimalNumber);
+        System.out.print(text3);
+        System.out.println();
+        System.out.println(text6);
+        System.out.println();
+        System.out.println(text4 + ":");
+        System.out.println(wholeNumber);
+        System.out.println(decimalNumber);  
+        System.out.println(text6);
+    }
+    
+}
